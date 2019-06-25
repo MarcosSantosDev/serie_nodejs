b9680610ca7f03d7be3f2ed967fcd8e461692f8a
marcos_santos <marcosadr034@gmail.com>
2019-06-25 00:32:16 -0300

    initial authenticate

--- src/controlers/authController.js
+++ src/controlers/authController.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/users');
 
@@ -24,5 +25,22 @@ router.post('/register', async (req, res) => {
         })
     }
 })
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+    const passwordCompare = await bcrypt.compare(password, user.password);
+
+    if(!user)
+        return res.status(400).send({ error: 'User not found' });
+    
+    if(!passwordCompare)
+        return res.status(400).send({ error: 'Invalid password' });
+
+    user.password = undefined;
+
+    res.send({ user });
+});
 
 module.exports = app => app.use('/auth', router);
